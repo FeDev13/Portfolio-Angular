92eca96341740c3631b75281d905c3007e41ddac
Federico Robledo <federicorobledo85@gmail.com>
2022-04-28 20:07:35 -0300

    second commit

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import { AngularBeeProgressBarModule } from 'angular-bee-progress-bar';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { RedesComponent } from './components/redes/redes.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AboutComponent } from './components/about/about.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { AcademicaComponent } from './components/academica/academica.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 
 @NgModule({
@@ -13,9 +22,18 @@ import { RedesComponent } from './components/redes/redes.component';
     HeaderComponent,
     LogoAPComponent,
     RedesComponent,
+    BannerComponent,
+    AboutComponent,
+    ExperienciaComponent,
+    AcademicaComponent,
+    SkillsComponent,
+    ProjectsComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatProgressBarModule,
+    AngularBeeProgressBarModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
